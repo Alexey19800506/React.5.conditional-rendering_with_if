7aca0092f7ef3d602b9e2589764cca825913a26b
Alexey19800506 <osiyuk19800506@gmail.com>
2023-10-11 11:54:51 +0300

    t1

---
+++ src/Auth.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Greeting from './Greeting';
+import Login from './Login';
+import Logout from './Logout';
+
+class Auth extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoggedIn: false,
+    };
+  }
+
+handleLogin = () => {
+  this.setState({
+    isLoggedIn: true,
+  });
+}
+
+handleLogout = () => {
+  this.setState({
+    isLoggedIn: false,
+  });
+}
+
+    render() {
+        return (
+          <div className='panel'>
+            <Greeting isLoggedIn={this.state.isLoggedIn}/>
+            {this.state.isLoggedIn ? (
+              <Logout onLogout={this.handleLogout}/>
+            ) : ( 
+              <Login onLogin={this.handleLogin}/>              
+            )}           
+          </div>
+        );
+    }
+}
+   
+export default Auth;
+
